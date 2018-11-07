48df1ff096ac069929005395e827ab3b2ed6fe73
XrebornX <44034423+XrebornX@users.noreply.github.com>
2018-11-07 16:46:44 +0800

    ver1.0
    introduce-ver1.0

---
+++ flow.js
@@ -0,0 +1,45 @@
+	var demoText = 'I have a dream, I hope i can  find a job before 11.11 ...';
+	// var demoText = 'I';
+	var i = 0;
+	function show(word){
+		var num = word.length;
+		var shower = word.substr(0,i);
+		document.getElementById('speak').innerHTML = shower;
+		i++;
+		if(i > num){
+			clearInterval(timer);
+			$('#speak').text('').css({
+				'display': 'none',
+			});
+			finalWord(introducr1);
+			finalWord(introducr2);
+			finalWord(introducr3);
+			finalWord(introducr4);
+			finalWord(introducr5);
+			finalWord(introducr7,'red',20);
+			finalWord(introducr8,'red',20);
+			$('#speak').css({
+			'position': 'relative',
+			'width': '80%',
+			'margin-left': '10%',
+			'margin-top': '26px'
+		}).slideDown(2500);
+		}
+	}
+	var timer = setInterval('show(demoText)',300);
+	var introducr1 = '1：能使用常用的办公软件、PS 、AutoCAD等。';
+	var introducr2 = '2：熟悉HTML语言，可以结合DIV+CSS对页面进行布局。';
+	var introducr3 = '3：熟悉javascript、jquery，能够通过Ajax实现页面的局部更新和跨域信息的接收，了解session、cookie。';
+	var introducr4 = '4：具备面向对象开发思想，能够使用TP3.2框架进行MVC模式的开发。';
+	var introducr5 = '5：了解mysql、redis。';
+	var introducr6 = '6：做过OA系统的权限管理功能、分页效果和微信小程序的布局，了解git的一些操作。';
+	var introducr7 = '如果你觉得我可能合适你们公司的话，希望你能给我个机会，如果你们是在招收培训学员的话，我有需要会自然会联系你们的，谢谢';
+	var introducr8 = 'QQ：1787139672  　　  email：1787139672@qq.com';
+	function finalWord(intr,col='#1A1A1A',ft=23){
+		$('<span><span>').text(intr).css({
+			'color': col,
+			'display':' block',
+			'font-size': ft+'px',
+			'padding': '16px'
+		}).appendTo('#speak');
+	};
