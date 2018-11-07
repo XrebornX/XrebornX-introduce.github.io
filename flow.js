680bfce319c424a000e91a3159c3f2793bca1651
XrebornX <44034423+XrebornX@users.noreply.github.com>
2018-11-07 18:39:28 +0800

    Update flow.js
    change some  words

--- flow.js
+++ flow.js
@@ -26,14 +26,14 @@
 		}).slideDown(2500);
 		}
 	}
-	var timer = setInterval('show(demoText)',300);
+	var timer = setInterval('show(demoText)',350);
 	var introducr1 = '1：能使用常用的办公软件、PS 、AutoCAD等。';
 	var introducr2 = '2：熟悉HTML语言，可以结合DIV+CSS对页面进行布局。';
 	var introducr3 = '3：熟悉javascript、jquery，能够通过Ajax实现页面的局部更新和跨域信息的接收，了解session、cookie。';
 	var introducr4 = '4：具备面向对象开发思想，能够使用TP3.2框架进行MVC模式的开发。';
 	var introducr5 = '5：了解mysql、redis。';
 	var introducr6 = '6：做过OA系统的权限管理功能、分页效果和微信小程序的布局，了解git的一些操作。';
-	var introducr7 = '如果你觉得我可能合适你们公司的话，希望你能给我个机会，如果你们是在招收培训学员的话，我有需要会自然会联系你们的，谢谢';
+	var introducr7 = '现希望能从事PHP或者web前端的工作，如果你觉得我可能合适你们公司的话，希望你能给我个机会，如果你们是在招收培训学员的话，我有需要自然会联系你们的，谢谢';
 	var introducr8 = 'QQ：1787139672  　　  email：1787139672@qq.com';
 	function finalWord(intr,col='#1A1A1A',ft=23){
 		$('<span><span>').text(intr).css({
